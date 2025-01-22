4c684cb2b24a66e6afa1b6d9f9e2e47c408a7faa
aq5092 <alijandanwh3@gmail.com>
2025-01-22 08:15:59 +0500

    database setting

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create a connection to the database
+engine = create_engine('sqlite:///database.db')
+Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
