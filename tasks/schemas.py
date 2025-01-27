172b5f4470aa59b1e758aea1f4e1250d982392d5
aq5092 <alijandanwh3@gmail.com>
2025-01-27 05:43:33 +0500

    chemas class changed

--- tasks/schemas.py
+++ tasks/schemas.py
@@ -1,6 +1,30 @@
-from pydantic import BaseModel
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from typing import List, Optional
+from users.schemas import UserBase
+class Base(DeclarativeBase):
+    pass 
 
+class TaskBase(Base):
+    __tablename__ = "otntask"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    task_name: Mapped[str] = mapped_column(String(30))
+    user_id: Mapped[int] = mapped_column(ForeignKey("UserBase.user_account.id"))
+    user: Mapped["UserBase"] = relationship(back_populates="tasks")
+    tasks: Mapped[List["TaskTypeBase"]]= relationship(back_populates="task_types", cascade="all, delete_orphan")
+
+
+
+
+class TaskTypeBase(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    task_type_name: Mapped[str] = mapped_column(String(30))
+    task_id: Mapped[int] = mapped_column(ForeignKey("otntask.id"))
+    task_types: Mapped["TaskBase"] = relationship(back_populates="tasks")
+    
+
+"""
 class TaskBase(BaseModel):
     task_name: str
     
@@ -15,3 +39,4 @@ class Task(TaskBase):
 
     class Config:
         orm_mode = True
+"""
--- users/schemas.py
+++ users/schemas.py
@@ -1,7 +1,26 @@
-from pydantic import BaseModel
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from typing import List, Optional
 
-class UserBase(BaseModel):
+class Base(DeclarativeBase):
+    pass
+
+class UserBase(Base):
+    __tablename__ = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(30))
+    tasks: Mapped[List["Tasks"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
+
+
+class UserCreate(Base):
+    name: str
+    email: str 
+
+
+""" class UserBase(BaseModel)
     username: str
     email: str
 
@@ -15,3 +34,4 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+ """
